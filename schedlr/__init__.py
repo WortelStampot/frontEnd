9fd8b2e848080b0c557ef60c0cb619fe33bc2d5d
Sil Demmer <Sil@Hey.com>
2023-10-20 16:59:22 -0400

    add application folder

---
+++ schedlr/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		)
+	
+	if test_config is None:
+		app.config.from_pyfile('config.py', silent=False)
+	else:
+		app.config.from_mapping(test_config)
+
+	#ensure the instance referenced above folder exists
+	try:
+		os.mkdirs(app.instance_path)
+	except OSError:
+		pass # do nothing?
+
+	@app.route('/start')
+	def a():
+		return 'frontEnd'
+	
+	return app
+
+	
