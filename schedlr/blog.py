15c62ff3d90acbf391178dbf9e1f9493f1f99765
Sil Demmer <Sil@Hey.com>
2023-10-22 22:42:34 -0400

    add get_post()

--- schedlr/blog.py
+++ schedlr/blog.py
@@ -1,12 +1,26 @@
 from flask import (
-    Blueprint, render_template, request, flash, g, redirect, url_for
+    Blueprint, render_template, request, flash, g, redirect, url_for, abort
 )
 
 from schedlr.db import get_db
-
 from schedlr.auth import login_required
 
 bp = Blueprint('blog', __name__)
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"post id {id} doesn't exist")
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    
+    return post
 
 @bp.route('/')
 def index():
