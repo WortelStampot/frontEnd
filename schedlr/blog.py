1a0d4b93a81c43f0d55025ce7737d3363dfd4c48
Sil Demmer <Sil@Hey.com>
2023-10-22 22:35:15 -0400

    blog.create route

--- schedlr/blog.py
+++ schedlr/blog.py
@@ -1,8 +1,10 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, flash, g, redirect, url_for
 )
 
 from schedlr.db import get_db
+
+from schedlr.auth import login_required
 
 bp = Blueprint('blog', __name__)
 
@@ -15,3 +17,27 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('POST', 'GET'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'title is required'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+        
+    return render_template('blog/create.html')
