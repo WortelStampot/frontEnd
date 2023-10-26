321c637f3a9a8b7f86d13e81dd08a978401cc733
Sil Demmer <Sil@Hey.com>
2023-10-26 11:52:46 -0400

    from tutor session: sorting schedule data

--- schedlr/table.py
+++ schedlr/table.py
@@ -12,7 +12,24 @@ def renderTable():
     with open('backEnd/dataSample.json') as f:
         scheduledata = f.read()
     schedule = json.loads(scheduledata) # wouldn't it be simpler to have access to the Staff, and Role objects instead of json string?
-    shifts = sorted(schedule['shifts'], key= lambda shift: shift['staff']) # sort shifts by staff name
-    staff = set(shift['staff'] for shift in shifts)
+    shiftsByStaff = {} #name: list of shifts
+    for shift in schedule["shifts"]:
+        #if name is already in shiftsByStaff, add shift to the list at that name
+        #if name is not in shiftsByStaff, set to empty list
+        name = shift["staff"]
+        shiftsByStaff.setdefault(name, [])
+        shiftsByStaff[name].append(shift)
 
-    return render_template('table/view.html', shifts=shifts, staff=staff)
+    data = []
+    for staff, shifts in shiftsByStaff.items():
+        staffDataDict = {"name": staff, "MONDAY": [], "TUESDAY": [], "WEDNESDAY": [], "THURSDAY": [], "FRIDAY": [], "SATURDAY": [], "SUNDAY": []}
+        for shift in shifts:
+            staffDataDict[shift["day"]].append(shift["role"])
+        staffData = []
+        for key, value in staffDataDict.items():
+            if key != "name":
+                value = ", ".join(value)
+            staffData.append(value)
+        data.append(staffData)
+
+    return render_template('table/view.html', data=data)
