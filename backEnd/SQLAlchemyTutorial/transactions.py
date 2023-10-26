e185153c05b7fca7f7a24a7d637d56c3cb2b7d3f
Sil Demmer <Sil@Hey.com>
2023-10-26 16:44:02 -0400

    using Session

--- backEnd/SQLAlchemyTutorial/transactions.py
+++ backEnd/SQLAlchemyTutorial/transactions.py
@@ -63,3 +63,53 @@ def mappingAccess(result):
         print(f'x: {x} y: {y}')
 
 fetchRows(tupleAssignment)
+
+
+# Sending Parameters
+def sendParameters():
+    with engine.connect() as connection:
+        result = connection.execute(
+            text('SELECT x,y FROM some_table WHERE y > :y'),
+            {'y': 2} #dict sets the value for ':y'
+        )
+        for row in result:
+            print(f'x: {row.x} y: {row.y}')
+
+sendParameters()
+
+def sendMultipleParameters():
+    '''
+    the SQL statement is invoked multiple times,
+    once for each parameter dictionary
+    '''
+    with engine.connect() as connection:
+        connection.execute(
+            text('INSERT INTO some_table (x, y) VALUES (:x, :y)'),
+            [   {'x': 11, 'y': 12}, {'x': 13, 'y':14}   ]
+        )
+        connection.commit()
+
+sendMultipleParameters()
+
+
+# Executing with an ORM Session
+from sqlalchemy.orm import Session
+
+def withSession():
+    statement = text('SELECT x,y FROM some_table WHERE y> :y ORDER BY x, y')
+    with Session(engine) as session:
+        result = session.execute(statement, {'y':6})
+        for row in result:
+            print(f'x: {row.x} y: {row.y}')
+
+withSession()
+
+def sessionUpdate():
+    with Session(engine) as session:
+        result = session.execute(
+            text('UPDATE some_table SET y= :y WHERE x= :x'),
+            [ {'x': 9, 'y': 11}, {'x': 13, 'y': 15} ]
+        )
+        session.commit()
+
+sessionUpdate()
