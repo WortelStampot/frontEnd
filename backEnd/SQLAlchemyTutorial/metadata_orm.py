7f1570578e77dd317aeb0c73ae7a15b2c8e83476
Sil Demmer <Sil@Hey.com>
2023-10-27 16:58:37 -0400

    table reflection

--- backEnd/SQLAlchemyTutorial/metadata_orm.py
+++ backEnd/SQLAlchemyTutorial/metadata_orm.py
@@ -61,3 +61,26 @@ CREATE TABLE address (
 ...
 COMMIT
 '''
+
+
+# Table Reflection
+from sqlalchemy import text, Table
+
+# create 'some_table' to reflect
+def commitChanges():
+    with engine.connect() as conn:
+        conn.execute(
+            text("CREATE TABLE some_table (x int, y int)") #create table
+            )
+        conn.execute(
+            text("INSERT INTO some_table (x,y) VALUES (:x, :y)"), # insert values into table
+            [{"x":1, "y":1}, {"x":2, "y":4}] #where these are the values for :x, :y 
+        )
+        conn.commit()
+
+commitChanges()
+
+# passing 'engine' to autoload_with says:
+# in this engine's database, look for a table named 'some_table',
+# reflect it, and store the reflection on this 'Base.metadata' MetaData object
+some_table = Table('some_table', Base.metadata, autoload_with=engine)
