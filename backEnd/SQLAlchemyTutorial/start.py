2e06fd684d640efce2cf7d867532165d98918c1b
Sil Demmer <Sil@Hey.com>
2023-10-27 22:10:41 -0400

    using a local database

--- backEnd/SQLAlchemyTutorial/start.py
+++ backEnd/SQLAlchemyTutorial/start.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+local_db = 'backEnd/SQLAlchemyTutorial/tutorial.sqlite'
+mem_db = ':memory:'
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-pass
+engine = create_engine(f"sqlite+pysqlite:///{mem_db}", echo=True)
