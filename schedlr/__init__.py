8bd10dc5139a4da670eec4c2ce2d2cb5c3c3e4a3
Sil Demmer <Sil@Hey.com>
2023-10-29 22:00:04 -0400

    toward the goal of getting an alchemy database up and running, all code in the __init__ file

--- schedlr/__init__.py
+++ schedlr/__init__.py
@@ -25,13 +25,65 @@ def create_app(test_config=None):
 	from . import db # initialize tutorial db
 	db.init_app(app)
 	
-	from .alchemy import alchemyDB # initialze sqlalchemy db
+	from flask_sqlalchemy import SQLAlchemy # initialze sqlalchemy db
+	from sqlalchemy.orm import DeclarativeBase
+
+	class Base(DeclarativeBase):
+		pass
+
+	alchemyDB = SQLAlchemy(model_class=Base)
 	alchemyDB.init_app(app)
 
+	from sqlalchemy import Integer, String
+	from sqlalchemy.orm import Mapped, mapped_column
+
+	# set up the database model
+	class User(alchemyDB.Model):
+		id: Mapped[int] = mapped_column(Integer, primary_key=True)
+		username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+		email: Mapped[str] = mapped_column(String)
+
+	# create tables
 	with app.app_context():
-		from .alchemy.models import User # may be redundent? may be a blunder?
-		'when you define models in other modules, import them before calling'
 		alchemyDB.create_all()
+
+	# query the data
+	from flask import render_template, request, redirect, url_for
+	@app.route('/users') #NOTE: seperate 'view' module? use Blueprint?
+	def user_list():
+		users = db.session.execute(
+			db.select(User).order_by(User.username)
+		)
+		return render_template('user/list.html', users=users)
+
+	@app.route('/user/create', methods=['POST', 'GET'])
+	def user_create():
+		if request.method == 'POST':
+			user = User(
+				username = request.form['username'],
+				email = request.form['email'],
+			)
+			db.session.add(user)
+			db.session.commit()
+			return redirect(url_for('user_detail', id=user.id))
+		
+		return render_template('user/create.html')
+
+	@app.route('/user/<int:id>')
+	def user_detail(id):
+		user = db.get_or_404(User, id)
+		return render_template('user/detail.html', user=user)
+
+	@app.route('/user/<int:id>/delete', methods=['POST', 'GET'])
+	def user_delete(id):
+		user = db.get_or_404(User, id)
+
+		if request.method == 'POST':
+			db.session.delete(user)
+			db.session.commit()
+			return redirect(url_for('user_list'))
+		
+		return render_template('user/delete.html', user=user)
 		
 	from . import auth
 	app.register_blueprint(auth.bp)
