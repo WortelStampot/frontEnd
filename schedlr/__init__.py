791054c9256c3de2b8ed78e320ebfbcf6bd99d91
Sil Demmer <Sil@Hey.com>
2023-10-29 22:05:57 -0400

    renamed SQLAlchemy instance

--- schedlr/__init__.py
+++ schedlr/__init__.py
@@ -51,8 +51,8 @@ def create_app(test_config=None):
 	from flask import render_template, request, redirect, url_for
 	@app.route('/users') #NOTE: seperate 'view' module? use Blueprint?
 	def user_list():
-		users = db.session.execute(
-			db.select(User).order_by(User.username)
+		users = alchemyDB.session.execute(
+			alchemyDB.select(User).order_by(User.username)
 		)
 		return render_template('user/list.html', users=users)
 
@@ -63,24 +63,24 @@ def create_app(test_config=None):
 				username = request.form['username'],
 				email = request.form['email'],
 			)
-			db.session.add(user)
-			db.session.commit()
+			alchemyDB.session.add(user)
+			alchemyDB.session.commit()
 			return redirect(url_for('user_detail', id=user.id))
 		
 		return render_template('user/create.html')
 
 	@app.route('/user/<int:id>')
 	def user_detail(id):
-		user = db.get_or_404(User, id)
+		user = alchemyDB.get_or_404(User, id)
 		return render_template('user/detail.html', user=user)
 
 	@app.route('/user/<int:id>/delete', methods=['POST', 'GET'])
 	def user_delete(id):
-		user = db.get_or_404(User, id)
+		user = alchemyDB.get_or_404(User, id)
 
 		if request.method == 'POST':
-			db.session.delete(user)
-			db.session.commit()
+			alchemyDB.session.delete(user)
+			alchemyDB.session.commit()
 			return redirect(url_for('user_list'))
 		
 		return render_template('user/delete.html', user=user)
