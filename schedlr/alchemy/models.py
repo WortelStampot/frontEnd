6d6559c17fa65bc905ae87dd1b114ccf9d07f5ce
Sil Demmer <Sil@Hey.com>
2023-10-29 22:01:59 -0400

    removed unsuccessful attempt at structuring

--- schedlr/alchemy/models.py
+++
@@ -1,9 +0,0 @@
-from .__init__ import alchemyDB # will this work?
-
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
-
-class User(alchemyDB.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String)
