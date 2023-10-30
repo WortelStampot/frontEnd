34657d6c402cca574bd92e8c6d8030dca77c346f
Sil Demmer <Sil@Hey.com>
2023-10-30 13:44:33 -0400

    structure for sqlalchemy with application factory from: https://github.com/flaskcwg/flask-docs-zh/blob/e86525172263f9429651b9d44270779e8ef3ba53/docs/patterns/appfactories.rst#factories--extensions

--- schedlr/__init__.py
+++ schedlr/__init__.py
@@ -22,69 +22,18 @@ def create_app(test_config=None):
 	except OSError: # when the folder already exists, there would be an error-
 		pass # so we 'do nothing' as a success
 
-
 	from . import db # initialize tutorial db
 	db.init_app(app)
 	
-	from flask_sqlalchemy import SQLAlchemy # initialze sqlalchemy db
-	from sqlalchemy.orm import DeclarativeBase
-
-	class Base(DeclarativeBase):
-		pass
-
-	alchemyDB = SQLAlchemy(model_class=Base)
+	from .model import alchemyDB # initialize sqlalchemy db
 	alchemyDB.init_app(app)
 
-	from sqlalchemy import Integer, String
-	from sqlalchemy.orm import Mapped, mapped_column
-
-	# set up the database model
-	class User(alchemyDB.Model):
-		id: Mapped[int] = mapped_column(Integer, primary_key=True)
-		username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-		email: Mapped[str] = mapped_column(String)
-
-	# create tables
+	# create alchemy tables
 	with app.app_context():
 		alchemyDB.create_all()
 
-	# query the data
-	from flask import render_template, request, redirect, url_for
-	@app.route('/users') #NOTE: seperate 'view' module? use Blueprint?
-	def user_list():
-		users = alchemyDB.session.execute(
-			alchemyDB.select(User).order_by(User.username)
-		)
-		return render_template('user/list.html', users=users)
-
-	@app.route('/user/create', methods=['POST', 'GET'])
-	def user_create():
-		if request.method == 'POST':
-			user = User(
-				username = request.form['username'],
-				email = request.form['email'],
-			)
-			alchemyDB.session.add(user)
-			alchemyDB.session.commit()
-			return redirect(url_for('user_detail', id=user.id))
-		
-		return render_template('user/create.html')
-
-	@app.route('/user/<int:id>')
-	def user_detail(id):
-		user = alchemyDB.get_or_404(User, id)
-		return render_template('user/detail.html', user=user)
-
-	@app.route('/user/<int:id>/delete', methods=['POST', 'GET'])
-	def user_delete(id):
-		user = alchemyDB.get_or_404(User, id)
-
-		if request.method == 'POST':
-			alchemyDB.session.delete(user)
-			alchemyDB.session.commit()
-			return redirect(url_for('user_list'))
-		
-		return render_template('user/delete.html', user=user)
+	from . import users
+	app.register_blueprint(users.blueprint)	
 		
 	from . import auth
 	app.register_blueprint(auth.bp)
